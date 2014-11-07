86db2c629107700c79c863ba204e152bc8c2f95c
grayyeargin <grayyeargin@gmail.com>
2014-11-07 15:20:45 -0500

    added navigation bar, corrected hangman guess glitch would lose even if right guess after 5th count, fixed

--- models/hangmangame.rb
+++ models/hangmangame.rb
@@ -7,24 +7,12 @@ class Hangmangame < ActiveRecord::Base
     if word.include?(letter)
       index_spots = word.split('').map.with_index {|c, ind| if c == letter then ind end}
       index_spots.map {|ind| if ind != nil then self.game_state[ind] = letter end}
-      # self.game_state == word ? victory = true : victory = false
-      victory = self.victory?
+      if self.game_state == word then victory = true end
       self.update(game_state: self.game_state, good_guesses: good_guesses+letter, last_guess: letter, victory: victory)
     #If bad guess, add letter to bad guesses and increase badguess_count
     else
-      # badguess_count == 5 ? victory = true : victory = false
-      victory = self.victory?
+      if badguess_count == 5 then victory = false end
       self.update(bad_guesses: bad_guesses+letter, badguess_count: badguess_count+1, last_guess: letter, victory: victory)
-    end
-  end
-
-  def victory?
-    if self.game_state == word
-      victory = true
-    elsif badguess_count == 5
-      victory = false
-    else
-      nil
     end
   end
 
